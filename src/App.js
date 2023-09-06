a2c7f0af58f04ddaf391efe998dec02334d453db
Shubham Tiwari <shubht3303@gmail.com>
2023-09-06 22:00:37 +0530

    Updating some of the main UI

--- src/App.js
+++ src/App.js
@@ -1,219 +1,151 @@
 import Navigation from "./UI/navigation";
 import NodeContainer from "./UI/nodeContainer";
-
 import React, { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
   const [reset, setReset] = useState(false);
-  // const [binSearch, setBinSearch] = useState(false);
   const [algorithmExplain, setAlgorithmExplain] = useState(false);
   const [found, setFound] = useState(false);
-  const [array, setarray] = useState([]);
+  const [array, setArray] = useState([]);
   const [resultArr, setResultArr] = useState([]);
-  const [i, setI] = useState(null);
-  const [condi, setCondi] = useState(null);
+  const [index, setIndex] = useState(null);
+  const [condition, setCondition] = useState(null);
 
-  let temp = [];
-  function updateArray() {
-    while (temp.length <= 39) {
-      let r = Math.round(Math.random() * 60 + 1);
-      if (temp.indexOf(r) === -1) {
+  const generateArray = () => {
+    const temp = [];
+    while (temp.length < 40) {
+      const r = Math.round(Math.random() * 60 + 1);
+      if (!temp.includes(r)) {
         temp.push(r);
-      } else {
-        continue;
       }
     }
-
-    console.log(temp.length);
-    setarray(temp);
+    setArray(temp);
     document.querySelector(".node_container").classList.remove("hidden");
-  }
+  };
 
-  // node container code for testing
-
-  // function color_reset(arr) {
-  //   console.log("triggered");
-
-  // });
-  // arr.forEach((element, i, path) => {
-  //   console.log(document.querySelector(`.node[uid = "${element}"]`));
-  //   document.querySelector(
-  //     `.node[uid = "${element}"]`
-  //   ).style.backgroundColor = "none";
-  //   document.querySelector(`.node[uid = "${element}"]`).style.border =
-  //     "none ";
-  // });
-  // }
-
-  const temp2 = [];
-  const temp3 = [];
-  let index, condition;
-
-  function BinarySearch(array, search_ele) {
+  const binarySearch = (array, searchEle) => {
     let left = 0;
-    let right = array.length;
+    let right = array.length - 1;
+    const steps = [];
 
     while (left <= right) {
-      let mid = Math.floor((left + right) / 2);
+      const mid = Math.floor((left + right) / 2);
+      const midEle = array[mid];
+      steps.push(midEle);
 
-      let mid_ele = array[mid];
-      console.log(mid_ele);
-      temp2.push(mid_ele);
-      if (mid_ele === search_ele) {
-        temp2.push(mid_ele);
-        return [true, mid];
-      } else if (search_ele < mid_ele) {
+      if (midEle === searchEle) {
+        return [true, mid, steps];
+      } else if (searchEle < midEle) {
         right = mid - 1;
-      } else if (search_ele > mid_ele) {
+      } else {
         left = mid + 1;
       }
     }
-    // setSearchClicked(false);
-    return [false];
-  }
+    return [false, null, steps];
+  };
 
-  function LinearSearch(array, search_ele) {
+  const linearSearch = (array, searchEle) => {
+    const steps = [];
     for (let i = 0; i < array.length; i++) {
-      if (array[i] === search_ele) {
-        temp3.push(array[i]);
-        return [true, i];
+      steps.push(array[i]);
+      if (array[i] === searchEle) {
+        return [true, i, steps];
       }
-      temp3.push(array[i]);
     }
-    return false;
-  }
+    return [false, null, steps];
+  };
 
-  function didReset(val) {
-    // setReset(val);
-    setReset(val);
-    setI("");
-    setCondi("");
+  const handleReset = () => {
+    setReset(true);
+    setIndex(null);
+    setCondition(null);
     document.querySelector(".analysis_div").classList.add("hidden");
-    // setBinSearch(false);
     window.location.reload();
-    // color_reset(resultArr);
-    // setTimeout(() => {
-    //   console.log("timeout");
-    // }, 1000);
-    // updateArray();
-  }
-  function update() {
-    updateArray();
-  }
+  };
 
-  function didSearch(val) {
-    // setBinSearch(val[0]);
-    let output;
-    if (val[1] === "linear") {
-      output = LinearSearch(array, array[19]);
-      if (output.length === 1) {
-        setFound(false);
+  const handleSearch = ([, searchType]) => {
+    const searchEle = searchType === "linear" ? array[19] : array[15];
+    const [isFound, idx, steps] =
+      searchType === "linear"
+        ? linearSearch(array, searchEle)
+        : binarySearch(array, searchEle);
+
+    setFound(isFound);
+    setResultArr(steps);
+    setIndex(idx);
+
+    if (isFound) {
+      let complexity;
+      if (searchType === "linear") {
+        complexity =
+          idx === 0
+            ? "Best Case O(1)"
+            : idx < array.length / 2
+            ? "Average Case <= O(n/2)"
+            : "Worst Case O(n)";
       } else {
-        index = output[1];
-        if (index + 1 === 1) {
-          condition = "Best Case O(1)";
-        } else if (index + 1 <= Math.round(array.length / 2)) {
-          condition = "Average Case <= O(n/2)";
-        } else {
-          condition = "Worst Case O(n)";
-        }
-
-        setFound(true);
-        setI(index);
-        setCondi(condition);
-
-        // console.log(temp2);
+        complexity =
+          idx === Math.floor(array.length / 2)
+            ? "Best Case O(1)"
+            : idx === 0 || idx === array.length - 1
+            ? "Worst Case O(log n)"
+            : "Average Case < O(log n)";
       }
-      setResultArr(temp3);
-    } else if (val[1] === "binary") {
-      output = BinarySearch(array, array[15]);
-      if (output.length === 1) {
-        setFound(false);
-      } else if (output.length === 2) {
-        index = output[1];
-        if (index + 1 === Math.round(array.length / 2)) {
-          condition = "Best Case O(1)";
-        } else if (index + 1 === 1 || index + 1 === array.length) {
-          condition = "Worst Case O(log n) ";
-        } else {
-          condition = "Average Case < O(log n)";
-        }
-        setFound(true);
-        setI(index);
-        setCondi(condition);
-        // console.log(temp2);
-      }
-      setResultArr(temp2);
+      setCondition(complexity);
     }
+
     document.querySelector(".analysis_div").classList.remove("hidden");
-    // console.log(temp2);
-    // setResultArr(temp2);
-    // console.log(resultArr);
-  }
+  };
 
-  function didExplainAlgorithm(val) {
-    setAlgorithmExplain(val);
-  }
+  const highlightNodes = () => {
+    if (!found) return;
 
-  useEffect(() => {
-    let val;
-    if (found === true) {
-      val = resultArr.pop();
-    }
-    resultArr.forEach((element, i, path) => {
+    const lastElement = resultArr.pop();
+    resultArr.forEach((element, i) => {
       setTimeout(() => {
-        console.log(document.querySelector(`.node[uid = "${element}"]`));
-        // setTimeout(() => {
-        //   document.querySelector(
-        //     `.node[uid = "${element}"]`
-        //   ).style.backgroundColor = "#B0087F";
-        // }, 500);
-
-        document.querySelector(
-          `.node[uid = "${element}"]`
-        ).style.backgroundColor = "#f06595";
-        document.querySelector(`.node[uid = "${element}"]`).style.border =
-          "1px solid #9c36b5 ";
-      }, 200 * i);
-
-      setTimeout(() => {
-        if (found === true) {
-          // let v = resultArr[resultArr.length - 1];
-          document.querySelector(
-            `.node[uid = "${val}"]`
-          ).style.backgroundColor = "#B0087F";
+        const node = document.querySelector(`.node[uid="${element}"]`);
+        if (node) {
+          node.style.backgroundColor = "#f06595";
+          node.style.border = "1px solid #9c36b5";
         }
-      }, 200 * resultArr.length);
+      }, 200 * i);
     });
-  }, [found, resultArr]);
+
+    setTimeout(() => {
+      const node = document.querySelector(`.node[uid="${lastElement}"]`);
+      if (node) {
+        node.style.backgroundColor = "#B0087F";
+      }
+    }, 200 * resultArr.length);
+  };
+
+  useEffect(highlightNodes, [found, resultArr]);
 
   useEffect(() => {
     document.querySelectorAll(".node").forEach((element) => {
       element.style.backgroundColor = "blueviolet";
     });
-    if (reset === true) {
+    if (reset) {
       setReset(false);
     }
   }, [reset]);
 
   return (
     <React.Fragment>
-      <div class="analysis_div hidden">
-        <div>Element found at index : {i}</div>
-        <div> Time Complexity Analysis : {condi}</div>
+      <div className="analysis_div hidden">
+        <div>Element found at index: {index}</div>
+        <div>Time Complexity Analysis: {condition}</div>
       </div>
-      <div className="container ">
+      <div className="container">
         <Navigation
-          onReset={didReset}
-          onSearch={didSearch}
-          onUpdate={update}
-          onExplainAlgorithm={didExplainAlgorithm}
+          onReset={handleReset}
+          onSearch={handleSearch}
+          onUpdate={generateArray}
+          onExplainAlgorithm={setAlgorithmExplain}
         />
         <NodeContainer
-          updatedArray={array.sort(function (a, b) {
-            return a - b;
-          })}
+          updatedArray={array.sort((a, b) => a - b)}
           state={true}
           isAlgorithmExplain={algorithmExplain}
         />
